d1f2d8714b337b15345af7ad335895395628a681
ShivamShrirao <shivamshrirao@gmail.com>
2021-05-18 20:45:34 +0530

    enc dec

--- model/conv_blocks.py
+++ model/conv_blocks.py
@@ -7,7 +7,7 @@ from .attention import SqueezeExcite, MHSA3D
 
 
 class NormAct(layers.Layer):
-    def __init__(activation=tf.nn.leaky_relu, norm='gn', gn_grps=8, **kwargs):
+    def __init__(self, activation=tf.nn.leaky_relu, norm='gn', gn_grps=8, **kwargs):
         super().__init__(**kwargs)
         if norm == 'gn':
             # gn_grps = compute_factors(gn_grps, filters, gn_grps)
@@ -24,16 +24,29 @@ class NormAct(layers.Layer):
 
 
 class ConvNorm(layers.Layer):
-    def __init__(filters, kernel_size=3, strides=1, groups=1, deconv=False, padding='same',
+    def __init__(self, filters, kernel_size=3, strides=1, groups=1, deconv=False, padding='same',
                  activation=tf.nn.leaky_relu, do_norm_act=True, norm='gn', gn_grps=8, **kwargs):
         super().__init__(**kwargs)
-        self.lyrs = list()
-        self.lyrs.append(layers.Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                                       use_bias=not do_norm_act, data_format="channels_first", groups=groups))
-        if do_norm_act:
-            self.lyrs.append(NormAct(activation=activation, norm=norm, gn_grps=gn_grps))
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.groups = groups
+        self.do_norm_act = do_norm_act
+        self.activation = activation
+        self.norm = norm
+        self.gn_grps = gn_grps
         if deconv:
             print("[!] DeConv not ported yet.")
+
+    def build(self, input_shape):
+        in_filters = input_shape[1]
+        self.lyrs = list()
+        self.groups = compute_factors(in_filters, self.filters, self.groups) # just to make sure filters divisible by groups
+        self.lyrs.append(layers.Conv3D(self.filters, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                                       use_bias=not self.do_norm_act, data_format="channels_first", groups=self.groups))
+        if self.do_norm_act:
+            self.lyrs.append(NormAct(activation=self.activation, norm=self.norm, gn_grps=self.gn_grps))
 
     def call(self, inp):
         x = inp
@@ -69,7 +82,7 @@ class AttnBottleneckBlock(layers.Layer):
         self.shortcut = self.get_shortcut(input_shape)
         self.net = self.get_bottleneck(input_shape)
 
-    def get_shortcut(input_shape):
+    def get_shortcut(self, input_shape):
         in_filters = input_shape[1]
         # operations for shortcut
         self.short_inp = True              # to decide input for shortcut, based on preact, filters and stride.
@@ -83,8 +96,7 @@ class AttnBottleneckBlock(layers.Layer):
                                   do_norm_act=False))
         return shortcut
 
-    def get_bottleneck(input_shape):
-        in_filters = input_shape[1]
+    def get_bottleneck(self, input_shape):
         inp = layers.Input(shape=input_shape[1:])
         # Main bottleneck network
         x = ConvNorm(self.filters, kernel_size=1, activation=self.activation, norm=self.norm)(inp)
@@ -92,13 +104,12 @@ class AttnBottleneckBlock(layers.Layer):
         if self.strides > 1:        # TODO: compare with strided convolution
             x = layers.AveragePooling3D(self.strides, data_format="channels_first")(x)
         if self.filters > 0:
-            self.groups = compute_factors(in_filters, filters, self.groups) # just to make sure filters divisible by groups
             x_s = ConvNorm(self.filters, kernel_size=3, activation=self.activation, norm=self.norm, groups=self.groups)(x)
         if self.dv > 0:
             x = MHSA3D(dv=self.dv, nheads=self.nheads)(x)
             x_s = layers.Concatenate(axis=1)([x, x_s]) if self.filters > 0 else x
 
-        x = NormAct(activation=activation, norm=self.norm)(x_s)
+        x = NormAct(activation=self.activation, norm=self.norm)(x_s)
 
         if self.squeeze_attn and self.dv==0:
             x = SqueezeExcite()(x)
--- model/network.py
+++ model/network.py
@@ -4,47 +4,49 @@ from tensorflow.keras import layers
 from .conv_blocks import ConvNorm, NormAct, AttnBottleneckBlock
 
 
-def down_stack(x, filters, blocks, strides=1, **kwargs):
-    x = AttnBottleneckBlock(filters, strides=strides, **kwargs)(x)
+def down_stack(x, filters, blocks, strides=1, frac_dv=0, **kwargs):
+    x = AttnBottleneckBlock(filters, dv=int(filters*frac_dv), strides=strides, **kwargs)(x)
 
     for i in range(1, blocks):
-        x = AttnBottleneckBlock(filters, **kwargs)(x)
+        x = AttnBottleneckBlock(filters, dv=int(filters*frac_dv), **kwargs)(x)
 
     return x
 
-def up_stack(x, skip, filters, blocks, strides=1, **kwargs):
+def up_stack(x, skip, filters, blocks, strides=1, frac_dv=0, **kwargs):
     if strides > 1:
         x = layers.UpSampling3D(data_format="channels_first")(x)
 
     x = layers.Concatenate(axis=1)([x, skip])
     for i in range(blocks):
-        x = AttnBottleneckBlock(filters, **kwargs)(x)
+        x = AttnBottleneckBlock(filters, dv=int(filters*frac_dv), **kwargs)(x)
     return x
 
 
-def enc_dec(x, stack_args):     # x(64,64,96,80)
-    x1 = down_stack(x ,  64, blocks=2, strides=2, **stack_args) # ( 256,32,48,40)
-    x2 = down_stack(x1, 128, blocks=2, strides=2, **stack_args) # ( 512,16,24,20)
-    x3 = down_stack(x2, 256, blocks=3, strides=2, **stack_args) # (1024, 8,12,10)
+def enc_dec(x, frac_dv, stack_args):     # x(64,64,96,80)
+    x1 = down_stack(x ,  64, blocks=2, strides=2, **stack_args)     # ( 256,32,48,40)
+    x2 = down_stack(x1, 128, blocks=2, strides=2, **stack_args)     # ( 512,16,24,20)
+    x3 = down_stack(x2, 256, blocks=3, strides=2, frac_dv=frac_dv, **stack_args) # (1024, 8,12,10)
+    x4 = down_stack(x3, 256, blocks=3, strides=2, frac_dv=frac_dv, **stack_args) # (1024, 4, 6, 5)
     # do norm act
-    y = up_stack(x3,x2, 256, blocks=3, strides=2, **stack_args) # (1024, 8,12,10)
-    y = up_stack(y, x1, 128, blocks=2, strides=2, **stack_args) # ( 512,16,24,20)
-    y = up_stack(y, x ,  64, blocks=2, strides=2, **stack_args) # ( 256,32,48,40)
+    y = up_stack(x4,x3, 256, blocks=3, strides=2, frac_dv=frac_dv, **stack_args) # (1024+1024->1024, 4, 6, 5)
+    y = up_stack(y, x2, 128, blocks=3, strides=2, frac_dv=frac_dv, **stack_args) # (1024+512->512,16,24,20)
+    y = up_stack(y, x1,  64, blocks=2, strides=2, **stack_args)     # ( 512+256->256,32,48,40)
+    y = up_stack(y, x ,  32, blocks=2, strides=2, **stack_args)     # ( 256+ 64->128,64,96,80)
     return y
 
 
 def build_network(cfg):
-    stack_args = {'activation': cfg['activation'], 'groups': cfg['groups'], 'norm': cfg['norm'], 'dv': cfg['dv'],
+    stack_args = {'activation': cfg['activation'], 'groups': cfg['groups'], 'norm': cfg['norm'],
                   'dp_rate': cfg['dp_rate'], 'dropout_type': cfg['dropout_type']}
 
     inp = layers.Input(shape=(4,depth,height,width))        # ( 4,128,192,160)
     x = inp
     x = ConvNorm(32, kernel_size=3, strides=2, activation=cfg['activation'], norm=cfg['norm'])(x)   # (32,64,96,80)
-    x = ConvNorm(64, kernel_size=3, activation=cfg['activation'], norm=cfg['norm'])(x)  # (64,64,96,80)
+    x = ConvNorm(64, kernel_size=3, activation=cfg['activation'], norm=cfg['norm'])(x)  # (64, 64, 96, 80)
 
-    x = enc_dec(x, stack_args)                                                          # ( 256,32,48,40)
+    x = enc_dec(x, cfg['frac_dv'], stack_args)                                          # (128,64, 96, 80)
 
-    x = layers.UpSampling3D(data_format="channels_first")(x)                            # (32, 64, 96, 80)
+    x = layers.UpSampling3D(data_format="channels_first")(x)                            # (128,128,192,160)
     x = ConvNorm(16, kernel_size=3, activation=cfg['activation'], norm=cfg['norm'])(x)  # (16,128,192,160)
     x = ConvNorm(3, kernel_size=3, activation=cfg['activation'], norm=cfg['norm'])(x)   # ( 3,128,192,160)
     x = layers.Softmax(axis=1)(x)         # softmax cause each pixel has unique class, no overlap with other classes, verified.
